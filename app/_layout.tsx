7d8c40498baac2a093a0f84ac65c26cfbcbdb8a3
Samarth Shrivastava <shrivastavasamarth22@gmail.com>
2024-04-03 09:19:06 +0530

    refactored RootLayout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,11 @@
+import Colors from "@/constants/Colors";
+import { Ionicons } from "@expo/vector-icons";
 import FontAwesome from "@expo/vector-icons/FontAwesome";
 import { useFonts } from "expo-font";
-import { Stack } from "expo-router";
+import { Stack, useRouter } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import { useEffect } from "react";
+import { TouchableOpacity } from "react-native";
 
 export {
 	// Catch any errors thrown by the Layout component.
@@ -12,11 +15,12 @@ export {
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
-export default function RootLayout() {
+const InitialLayout = () => {
 	const [loaded, error] = useFonts({
 		SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
 		...FontAwesome.font,
 	});
+	const router = useRouter();
 
 	// Expo Router uses Error Boundaries to catch errors in the navigation tree.
 	useEffect(() => {
@@ -33,13 +37,29 @@ export default function RootLayout() {
 		return null;
 	}
 
-	return <RootLayoutNav />;
-}
-
-function RootLayoutNav() {
 	return (
 		<Stack>
 			<Stack.Screen name="index" options={{ headerShown: false }} />
+			<Stack.Screen
+				name="signup"
+				options={{
+					title: "",
+					headerBackTitle: "",
+					headerShadowVisible: false,
+					headerStyle: { backgroundColor: Colors.background },
+					headerLeft: () => (
+						<TouchableOpacity onPress={router.back}>
+							<Ionicons name="arrow-back" size={24} color={Colors.dark} />
+						</TouchableOpacity>
+					),
+				}}
+			/>
 		</Stack>
 	);
-}
+};
+
+const RootLayoutNav = () => {
+	return <InitialLayout />;
+};
+
+export default RootLayoutNav;
