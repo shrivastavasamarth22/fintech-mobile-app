6411e280ab9dd4a480506835ec7650b5e68efa06
Samarth Shrivastava <shrivastavasamarth22@gmail.com>
2024-04-03 11:08:40 +0530

    integrated clerk into the layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -9,6 +9,30 @@ import { useEffect } from "react";
 import { TouchableOpacity } from "react-native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 
+import { ClerkProvider } from "@clerk/clerk-expo";
+
+import * as SecureStore from "expo-secure-store";
+
+const CLERK_PUBLISHABLE_KEY = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!;
+
+// Cache the Clerk JWT
+const tokenCache = {
+	async getToken(key: string) {
+		try {
+			return SecureStore.getItemAsync(key);
+		} catch (err) {
+			return null;
+		}
+	},
+	async saveToken(key: string, value: string) {
+		try {
+			return SecureStore.setItemAsync(key, value);
+		} catch (err) {
+			return;
+		}
+	},
+};
+
 export {
 	// Catch any errors thrown by the Layout component.
 	ErrorBoundary,
@@ -86,10 +110,15 @@ const InitialLayout = () => {
 
 const RootLayoutNav = () => {
 	return (
-		<GestureHandlerRootView style={{ flex: 1 }}>
-			<StatusBar style="light" />
-			<InitialLayout />
-		</GestureHandlerRootView>
+		<ClerkProvider
+			publishableKey={CLERK_PUBLISHABLE_KEY}
+			tokenCache={tokenCache}
+		>
+			<GestureHandlerRootView style={{ flex: 1 }}>
+				<StatusBar style="light" />
+				<InitialLayout />
+			</GestureHandlerRootView>
+		</ClerkProvider>
 	);
 };
 
